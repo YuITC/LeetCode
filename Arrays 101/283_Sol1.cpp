6d7fdb82672da90e1760370a011c0a1b65291273
Nguyen Phu Tai <111862377+YuITC@users.noreply.github.com>
2022-10-24 19:09:10 +0700

    Update 283_Sol1.cpp

--- Arrays 101/283_Sol1.cpp
+++ Arrays 101/283_Sol1.cpp
@@ -4,24 +4,9 @@
 // Date   		: 10-24-2022
 
 /***************************************************************************************************** 
- *
- * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements. 
- * Note that you must do this in-place without making a copy of the array. 
- * 
+ * Solution 1: Brute Force O(n^2)
  
- * Example 1:
- 	* Input: nums = [0,1,0,3,12]
- 	* Output: [1,3,12,0,0]
-	
- * Example 2:
- 	* Input: nums = [0]
- 	* Output: [0]
- 
- * Constraints:
- 	* 1 <= nums.length <= 104
- 	* -231 <= nums[i] <= 231 - 1
  ******************************************************************************************************/
-// Solution 1: Brute Force O(n^2)
 
 class Solution {
 public:
